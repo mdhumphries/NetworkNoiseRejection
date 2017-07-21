8cb166f752dfda01a70964a8056005464a0b6257
Mark Humphries <drmdhumphries+github@gmail.com>
2017-07-21 15:20:11 +0100

    Started Allen colourmap checking

--- Figures/Check_Allen_Results.m
+++ Figures/Check_Allen_Results.m
@@ -7,3 +7,12 @@ load('../Networks/Allen_mouse_brain_atlas/allen_colours.mat');  % colour-coding 
 
 load('../Results/Rejected_Allen_Gene_Leaf.mat');
 load('../Results/Clustered_Allen_Gene_Leaf.mat');
+
+[srtedGrps,IDs] = sort(Full.QmaxCluster,'ascend');
+
+origIDs = leafIDs_final(IDs);
+C = zeros(numel(IDs),1,3);
+C(:,:,1:3) = allen_colours(origIDs,:) ./ 255;
+
+figure
+image(C)
