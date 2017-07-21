ebd364fc1a655b0c7e3a45d248451e485073ccc2
Mark Humphries <drmdhumphries+github@gmail.com>
2017-07-21 12:45:18 +0100

    Save all synthetic data parameters

--- batch_planted_noise.m
+++ batch_planted_noise.m
@@ -6,14 +6,18 @@ clear all; close all
 addpath('ZhangNewman2015/');
 
 %% params for WSBM model
+%% communities
+Communities.size = 50;
+Communities.N = 3;
+
 %static
-cc=[100, 20];       % within communities
+Communities.cc=[100, 20];       % within communities
 
 
 %looping
-fraction_periphery_grid=[0.2 0.5 0.8 1.0];
-gg_cc_grid=[0.1 0.3 0.5 0.7 0.9];   % between communities: proportion of within communities' weight parameters
-cp_gg_grid=[0.1 0.3 0.5 0.7 0.9 1];   % from periphery to communities: proportion of between communities' weight parameters
+Communities.fraction_periphery_grid=[0.2 0.5 0.8 1.0];
+Communities.gg_cc_grid=[0.1 0.3 0.5 0.7 0.9];   % between communities: proportion of within communities' weight parameters
+Communities.cp_gg_grid=[0.1 0.3 0.5 0.7 0.9 1];   % from periphery to communities: proportion of between communities' weight parameters
 
 %% params for noise rejection
 
@@ -41,22 +45,22 @@ core_perf=table;
 
 
 %% set loops for params
-for count_peri=1:numel(fraction_periphery_grid)
-    fraction_periphery=fraction_periphery_grid(count_peri);
-    for count_gg_cc=1:numel(gg_cc_grid)
-        gg_cc=gg_cc_grid(count_gg_cc);
+for count_peri=1:numel(Communities.fraction_periphery_grid)
+    fraction_periphery=Communities.fraction_periphery_grid(count_peri);
+    for count_gg_cc=1:numel(Communities.gg_cc_grid)
+        gg_cc=Communities.gg_cc_grid(count_gg_cc);
         mixing_index=gg_cc;
-        for count_cp_gg=1:numel(cp_gg_grid)
+        for count_cp_gg=1:numel(Communities.cp_gg_grid)
             count_cp_gg
-            cp_gg=cp_gg_grid(count_cp_gg);
+            cp_gg=Communities.cp_gg_grid(count_cp_gg);
             noise_index=cp_gg;
-            mixing=mixing_index * cc;
+            mixing=mixing_index * Communities.cc;
             noise=noise_index * mixing; 
             pp=noise/2;
             %generate planted noise network
-            options.weight_dist=struct('ingroup', cc, 'outgroup', mixing,...
+            options.weight_dist=struct('ingroup', Communities.cc, 'outgroup', mixing,...
                 'periphery_periphery', pp, 'periphery_core', noise);
-            network=test_noise_rejection_planted_noise(50,3, 'low',fraction_periphery, options);
+            network=test_noise_rejection_planted_noise(Communities.size,Communities.N, 'low',fraction_periphery, options);
             % unit analyses
             
             %%% HERE: replace with function based on main template
@@ -110,5 +114,5 @@ end
 
 fname = datestr(now,30);
 
-save(['Results/WSBM_' fname],'core_perf','pars','optionsModel','optionsReject','clusterpars')
+save(['Results/WSBM_' fname],'core_perf','Communities','pars','optionsModel','optionsReject','clusterpars')
 
