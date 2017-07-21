7a1581a32f2b4b08901d2c05c1cfccbbbc0b4890
Mark Humphries <drmdhumphries+github@gmail.com>
2017-07-21 12:31:24 +0100

    First synthetic batch results

--- Visualise_Clusterings.m
+++ Visualise_Clusterings.m
@@ -3,8 +3,8 @@
 % Mark Humphries, Mat Evans 28/2/2017
 
 clear all; close all
-% fname = 'Allen_Gene_Leaf'; 
-fname = 'LesMis'; 
+fname = 'Allen_Gene_Leaf'; 
+% fname = 'LesMis'; 
 
 blnLabels = 1;      % write node labels? Omit for large networks
 fontsize = 6;
