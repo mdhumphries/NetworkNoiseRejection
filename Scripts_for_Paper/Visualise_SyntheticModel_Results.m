2c39da2bfbf5793570e141465489275392cb291c
mdhumphries <drmdhumphries+github@gmail.com>
2018-06-04 15:29:24 +0100

    part fix of visualisation

--- Scripts_for_Paper/Visualise_SyntheticModel_Results.m
+++ Scripts_for_Paper/Visualise_SyntheticModel_Results.m
@@ -5,13 +5,15 @@ clear all; close all;
 
 addpath('../Helper_Functions/')
 
-% fname = 'SyntheticEqual_NoNoise_20170808T130427';
-fname = 'SyntheticUnequal_NoNoise_20170808T175814';
+fname = 'SyntheticEqual_NoNoise_20180604T150903';
+blnCluster = 0;  % if done clustering, set this to 1
 
+% load results and set maps
 load(['../Results/' fname]);  % Main table is 'core_perf'
 
 nAlpha = numel(Model.alpha_range);  % split of weights between within and between
 nP = numel(Model.P_of_within);    % within communities P
+nBatch = size(Results.Time,3);   % number of model repeats
 
 % colormaps
 perf_cmap = brewermap(10,'Blues');
@@ -19,17 +21,20 @@ VI_cmap = brewermap(10,'Reds');
 
 %% rescaling of axes for Pcolor plots
 xsc = [Model.alpha_range 2*Model.alpha_range(end) - Model.alpha_range(end-1)];
+xtick = Model.alpha_range + diff(xsc)/2;
+
 ysc = [Model.P_of_within 2*Model.P_of_within(end) - Model.P_of_within(end-1)];
+ytick = Model.P_of_within + diff(ysc)/2;
+
 % this part of rescaling solution from: https://uk.mathworks.com/matlabcentral/newsreader/view_thread/30404
 % xsc = (xsc-Model.alpha_range(1))/(xsc(1)-xsc(end))*(Model.alpha_range(1)-Model.alpha_range(end)) + Model.alpha_range(1); % scale proportionally
 % ysc = (ysc-Model.P_of_within(1))/(ysc(1)-ysc(end))*(Model.P_of_within(1)-Model.P_of_within(end)) + Model.P_of_within(1); % scale proportionally
 
-% or just properly label each cell with appropriate value
-xtick = Model.alpha_range + diff(xsc)/2;
-ytick = Model.P_of_within + diff(ysc)/2;
 
 %% processing time
-plotmap = [Results.Time Results.Time(:,end)]; 
+meanTime = mean(Results.Time,3);
+% padding for PColor function
+plotmap = [meanTime meanTime(:,end)]; 
 plotmap = [plotmap; plotmap(end,:)];
 
 figure
@@ -94,58 +99,65 @@ xlabel('Proportion of strength within community')
 ylabel('P(within)')
 title('Number of groups (Config all positive eigs)')
 
+
 %% VI of clustering
-figure
+if blnCluster
+    % load clustering
+    load(['../Results/Clustering' fname]);  % Main table is 'core_perf'
 
-subplot(221),
-plotmap = [Results.nVIFull_QmaxSpectra Results.nVIFull_QmaxSpectra(:,end)]; 
-plotmap = [plotmap; plotmap(end,:)];
-colormap(VI_cmap)
-pcolor(xsc,ysc,plotmap);
-set(gca,'XTick',xtick,'XTickLabel',Model.alpha_range)
-set(gca,'YTick',ytick,'YTickLabel',Model.P_of_within)
-colorbar
-xlabel('Proportion of strength within community')
-ylabel('P(within)')
-title('VI Q max solution')
+    % plot clustering
+    figure
 
-subplot(222),
-plotmap = [Results.nVIFull_ConsensusSpectra Results.nVIFull_ConsensusSpectra(:,end)]; 
-plotmap = [plotmap; plotmap(end,:)];
-colormap(VI_cmap)
-pcolor(xsc,ysc,plotmap);
-set(gca,'XTick',xtick,'XTickLabel',Model.alpha_range)
-set(gca,'YTick',ytick,'YTickLabel',Model.P_of_within)
-colorbar
-xlabel('Proportion of strength within community')
-ylabel('P(within)')
-title('VI Consensus')
+    subplot(221),
+    plotmap = [Results.nVIFull_QmaxSpectra Results.nVIFull_QmaxSpectra(:,end)]; 
+    plotmap = [plotmap; plotmap(end,:)];
+    colormap(VI_cmap)
+    pcolor(xsc,ysc,plotmap);
+    set(gca,'XTick',xtick,'XTickLabel',Model.alpha_range)
+    set(gca,'YTick',ytick,'YTickLabel',Model.P_of_within)
+    colorbar
+    xlabel('Proportion of strength within community')
+    ylabel('P(within)')
+    title('VI Q max solution')
 
-subplot(223),
-plotmap = [Results.nVIFull_LouvainMin Results.nVIFull_LouvainMin(:,end)]; 
-plotmap = [plotmap; plotmap(end,:)];
-colormap(VI_cmap)
-pcolor(xsc,ysc,plotmap);
-set(gca,'XTick',xtick,'XTickLabel',Model.alpha_range)
-set(gca,'YTick',ytick,'YTickLabel',Model.P_of_within)
-colorbar
-xlabel('Proportion of strength within community')
-ylabel('P(within)')
-title('VI best Louvain')
+    subplot(222),
+    plotmap = [Results.nVIFull_ConsensusSpectra Results.nVIFull_ConsensusSpectra(:,end)]; 
+    plotmap = [plotmap; plotmap(end,:)];
+    colormap(VI_cmap)
+    pcolor(xsc,ysc,plotmap);
+    set(gca,'XTick',xtick,'XTickLabel',Model.alpha_range)
+    set(gca,'YTick',ytick,'YTickLabel',Model.P_of_within)
+    colorbar
+    xlabel('Proportion of strength within community')
+    ylabel('P(within)')
+    title('VI Consensus')
 
-subplot(224),
-plotmap = [Results.nVIFull_Multiway Results.nVIFull_Multiway(:,end)]; 
-plotmap = [plotmap; plotmap(end,:)];
-colormap(VI_cmap)
-pcolor(xsc,ysc,plotmap);
-set(gca,'XTick',xtick,'XTickLabel',Model.alpha_range)
-set(gca,'YTick',ytick,'YTickLabel',Model.P_of_within)
-colorbar
-xlabel('Proportion of strength within community')
-ylabel('P(within)')
-title('VI Multiway')
+    subplot(223),
+    plotmap = [Results.nVIFull_LouvainMin Results.nVIFull_LouvainMin(:,end)]; 
+    plotmap = [plotmap; plotmap(end,:)];
+    colormap(VI_cmap)
+    pcolor(xsc,ysc,plotmap);
+    set(gca,'XTick',xtick,'XTickLabel',Model.alpha_range)
+    set(gca,'YTick',ytick,'YTickLabel',Model.P_of_within)
+    colorbar
+    xlabel('Proportion of strength within community')
+    ylabel('P(within)')
+    title('VI best Louvain')
 
-% recale to all have the same colour-scale
-h=findobj(gcf,'type','axes');
-q=get(h,'clim');
-set(h,'clim',[min([q{:}]) max([q{:}])])
+    subplot(224),
+    plotmap = [Results.nVIFull_Multiway Results.nVIFull_Multiway(:,end)]; 
+    plotmap = [plotmap; plotmap(end,:)];
+    colormap(VI_cmap)
+    pcolor(xsc,ysc,plotmap);
+    set(gca,'XTick',xtick,'XTickLabel',Model.alpha_range)
+    set(gca,'YTick',ytick,'YTickLabel',Model.P_of_within)
+    colorbar
+    xlabel('Proportion of strength within community')
+    ylabel('P(within)')
+    title('VI Multiway')
+
+    % recale to all have the same colour-scale
+    h=findobj(gcf,'type','axes');
+    q=get(h,'clim');
+    set(h,'clim',[min([q{:}]) max([q{:}])])
+end
