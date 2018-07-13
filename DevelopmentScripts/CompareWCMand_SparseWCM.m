459895bc0a2d0f5311ebd44243e55c653e8bc0fd
mdhumphries <drmdhumphries+git@gmail.com>
2018-07-13 16:29:14 +0100

    testing null model functions

--- DevelopmentScripts/CompareWCMand_SparseWCM.m
+++ DevelopmentScripts/CompareWCMand_SparseWCM.m
@@ -1,13 +1,20 @@
 % script to test all null models...
- clearvars
- 
- load '../Networks/LesMis';
- 
+clearvars
+
+addpath ../Helper_Functions/
+
+load '../Networks/LesMis';
+A = full(Problem.A);
+pars.C = 1;             % conversion factor for real-valued weights (set=1 for integers)
+
+load Sep2711da01_40_120s_Sxy_Gaussian_1s.mat
+A = Sxyall{1};
+pars.C = 100;
+
  % analysis parameters
 pars.N = 100;           % repeats of sampling
 % pars.alpha = 0; %0.95; % 0.95; % 0;         % confidence interval on estimate of maxiumum eigenvalue for null model; set to 0 for mean
 pars.I = 0;      % interval: set to 0 for mean
-pars.C = 1;             % conversion factor for real-valued weights (set=1 for integers)
 pars.eg_min = 1e-2;      % given machine error, what is acceptable as "zero" eigenvalue
 
 % null model options
@@ -17,18 +24,22 @@ optionsModel.NoLoops = 1;     % prevent self-loops in the null model?
 
 %% run each null model...
 
-[linkFull.E,linkFull.D,linkFull.V,linkFull.A] = linkFullWCM(Problem.A,pars.N);
+[linkFull.E,linkFull.D,linkFull.V,linkFull.A] = linkFullWCM(A,pars.N,pars.C);
 
-[poissonFull.E,poissonFull.D,poissonFull.V,poissonFull.A] = poissonFullWCM(Problem.A,pars.N);
+[poissonFull.E,poissonFull.D,poissonFull.V,poissonFull.A] = poissonFullWCM(A,pars.N,pars.C);
 
-[linkSparse.E,linkSparse.D,linkSparse.V,linksparse.ExpA,linkSparse.A] = linkSparseWCM(Problem.A,pars.N,[],optionsModel);
+[linkSparse.E,linkSparse.D,linkSparse.V,linksparse.ExpA,linkSparse.A] = linkSparseWCM(A,pars.N,pars.C,optionsModel);
 
-[poissonSparse.E,poissonSparse.D,poissonSparse.V,poissonsparse.ExpA,poissonSparse.A] = poissonSparseWCM(Problem.A,pars.N,[],optionsModel);
+[poissonSparse.E,poissonSparse.D,poissonSparse.V,poissonsparse.ExpA,poissonSparse.A] = poissonSparseWCM(A,pars.N,pars.C,optionsModel);
 
 
 %% check output
+plotY = cell(4,1);
+plotY{1} = [linkFull.D(:).dStotal];
+plotY{2} = [poissonFull.D(:).dStotal];
+plotY{3} = [linkSparse.D(:).dStotal];
+plotY{4} = [poissonSparse.D(:).dStotal];
 
-for i = 1:pars.N
-    
-end
+figure
+UnpairedUnivariateScatterPlots(gca,plotY,'strX',{'linkF','PoissF','linkS','PoissS'});
 
